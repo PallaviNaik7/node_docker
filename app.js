92bdca5c770faf579629114684c10a7c174eabd0
PallaviNaik7 <pallavi.n@mrinq.com>
2022-02-18 13:07:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+module.exports = app;
+
+app.get("/hello", (request, response) => {
+  response.status(200).send({ type: "success", message: "Hello" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
