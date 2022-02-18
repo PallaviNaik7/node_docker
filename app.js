90003c888cfcadfe3a0f4ce9d61562ea3906e16a
PallaviNaik7 <pallavi.n@mrinq.com>
2022-02-18 17:33:07 +0530

    shutdown

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ app.get("/test/hello", (request, response) => {
   response.status(200).send({ type: "success", message: "Hello" });
 });
 
+/*    In app.js of all services   */
+process.on("SIGTERM", function () {
+  // monitor for SIGTERM(^C)
+  console.log("Shutting down service.");
+
+  // Graceful shutdown: can close sockets, close DB connections, clean up resources etc.
+
+  process.exit(); // process SHOULD exit by itself, after the OS sends kill code.
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
